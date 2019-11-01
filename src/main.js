1ad509723c52677c8b4ed648b5cafd3d6a68b4e7
Andrey Kovalyov <maclerkov@gmail.com>
2019-11-01 18:10:50 +0300

    На ворке

--- src/main.js
+++ src/main.js
@@ -1,13 +1,46 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import dateFilter from "./filters/date.filter";
+import currencyFilter from "./filters/currency.filter";
+import messagePlugin from "./utils/message.plugin";
+import Loader from "./components/app/Loader";
+import './registerServiceWorker'
+import 'materialize-css/dist/js/materialize.min'
+
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+Vue.use(messagePlugin)
+Vue.use(Vuelidate)
+Vue.filter('date', dateFilter)
+Vue.filter('currency', currencyFilter)
+Vue.component('Loader', Loader)
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBWBtwAdJntatLhr6EPlKsEIypCA-pBH_M",
+  authDomain: "vue-crm-a74f6.firebaseapp.com",
+  databaseURL: "https://vue-crm-a74f6.firebaseio.com",
+  projectId: "vue-crm-a74f6",
+  storageBucket: "vue-crm-a74f6.appspot.com",
+  messagingSenderId: "154059872174",
+  appId: "1:154059872174:web:822a31cb84bb6775955a3f",
+  measurementId: "G-Q7PCSFFYZ5"
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if(!app){
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+ <template>
+   <div>
+     <div class="page-title">
+       <h3>Счет</h3>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+       <button class="btn waves-effect waves-light btn-small" @click="refresh">
+         <i class="material-icons">refresh</i>
+       </button>
+     </div>
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+     <Loader v-if="loading"/>
+
+     <div v-else class="row">
+       <HomeBill
+         :rates="currency.rates"
+       />
+       <HomeCurrency
+         :rates="currency.rates"
+         :date="currency.date"
+       />
+     </div>
+   </div>
+ </template>
+
+ <script>
+   import HomeBill from '../components/HomeBill'
+   import HomeCurrency from '../components/HomeCurrency'
+   export default {
+     name: 'home',
+     data: () => ({
+       loading: true,
+       currency: null
+     }),
+     async mounted() {
+       this.currency = await this.$store.dispatch('fetchCurrency')
+       this.loading = false
+     },
+     components: {
+       HomeBill, HomeCurrency
+     },
+     methods: {
+       async refresh(){
+         this.loading = true
+         this.currency = await this.$store.dispatch('fetchCurrency')
+         this.loading = false
+       }
+     }
+   }
+ </script>
